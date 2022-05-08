e5e4cf7b4fe1f101859d88baccbc29062ae4c448
jelelahmed <abduljelelahmed>
2022-05-08 20:40:41 +0100

    updating our directory page with directory component and category item component

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Directory from './components/directory/directory.component';
+
+import './App.css';
+
+const App = () => {
+	return (
+		<div className='categories-container'>
+			<Directory />
+		</div>
+	)
+}
+export default App;
