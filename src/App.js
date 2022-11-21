84e366d5cbc8bd75bf09a50d0644230ca08680da
jelelahmed <abduljelelahmed>
2022-11-21 01:23:32 +0100

    finishing up the sign in functionality and the authentication page styling

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import {  Route, Routes } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
-import SignIn from './routes/sign-in/sign-in.component';
-
+import Authentication from './routes/authentication/authentication';
 import './App.css';
 
 
@@ -18,7 +17,7 @@ const App = () => {
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
 				<Route path='shop' element={<ShopPage />} />
-				<Route path='sign-in' element={<SignIn />} />
+				<Route path='auth' element={<Authentication />} />
 			</Route>
 		</Routes>
 	)
