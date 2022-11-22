78cf2b4f0e6012a01a1bb3b240b421a543f4d81b
jelelahmed <abduljelelahmed>
2022-11-22 22:40:59 +0100

    created product contexts and product card components

--- src/App.js
+++ src/App.js
@@ -4,19 +4,16 @@ import {  Route, Routes } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication';
+import Shop from './routes/shop/shop.component';
 import './App.css';
 
-
-const ShopPage = () => (
-	<div>'I am the shop page'</div>
-)
 
 const App = () => {
 	return (
 		<Routes>
 			<Route path='/' element={<Navigation />}>
 				<Route index element={<Home />} />
-				<Route path='shop' element={<ShopPage />} />
+				<Route path='shop' element={<Shop />} />
 				<Route path='auth' element={<Authentication />} />
 			</Route>
 		</Routes>
